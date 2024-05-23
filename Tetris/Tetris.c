714e8d2bf9629f132a0ff75453fa0b3b86ad0a5a
vincent9457 <26395137vin@gmail.com>
2024-05-23 11:44:52 +0800

    20240523

--- Tetris/Tetris.c
+++ Tetris/Tetris.c
@@ -4,9 +4,26 @@
 #include <time.h>
 #include <windows.h>
 
-
 #define CANVAS_WIDTH 10
 #define CANVAS_HEIGHT 20
+
+//掉落時間
+#define FALL_DELAY 500
+#define RENDER_DELAY 100
+
+//鍵盤對照表
+#define LEFT_KEY 0x25
+#define RIGHT_KEY 0x27 
+#define ROTATE_KEY 0x26 
+#define DOWN_KEY 0x28 
+#define FALL_KEY 0x20 
+
+// 判斷是否有按下按鈕的函式
+#define LEFT_FUNC() GetAsyncKeyState(LEFT_KEY) & 0x8000
+#define RIGHT_FUNC() GetAsyncKeyState(RIGHT_KEY) & 0x8000
+#define ROTATE_FUNC() GetAsyncKeyState(ROTATE_KEY) & 0x8000
+#define DOWN_FUNC() GetAsyncKeyState(DOWN_KEY) & 0x8000
+#define FALL_FUNC() GetAsyncKeyState(FALL_KEY) & 0x8000
 
 typedef enum
 {
@@ -197,27 +214,33 @@ void resetBlock(Block* block)
 void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
 {
     printf("\033[0;0H\n");
-    for (int i = 0; i < CANVAS_HEIGHT; i++) {
+    for (int i = 0; i < CANVAS_HEIGHT; i++)
+    {
         printf("|");
-        for (int j = 0; j < CANVAS_WIDTH; j++) {
+        for (int j = 0; j < CANVAS_WIDTH; j++)
+        {
             printf("\033[%dm\u3000", canvas[i][j].color);
         }
         printf("\033[0m|\n");
     }
 
-    //輸出Next:
+    // 輸出Next:
     printf("\033[%d;%dHNext:", 3, CANVAS_WIDTH * 2 + 5);
-    //輸出有甚麼方塊
+    // 輸出有甚麼方塊
     for (int i = 1; i <= 3; i++)
     {
         Shape shapeData = shapes[state->queue[i]];
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < 4; j++)
+        {
             printf("\033[%d;%dH", i * 4 + j, CANVAS_WIDTH * 2 + 15);
-            for (int k = 0; k < 4; k++) {
-                if (j < shapeData.size && k < shapeData.size && shapeData.rotates[0][j][k]) {
+            for (int k = 0; k < 4; k++)
+            {
+                if (j < shapeData.size && k < shapeData.size && shapeData.rotates[0][j][k])
+                {
                     printf("\x1b[%dm  ", shapeData.color);
                 }
-                else {
+                else
+                {
                     printf("\x1b[0m  ");
                 }
             }
@@ -226,20 +249,26 @@ void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
     return;
 }
 
-bool move(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int originalX, int originalY, int originalRotate, int newX, int newY, int newRotate, ShapeId shapeId) {
+bool move(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int originalX, int originalY, int originalRotate, int newX, int newY, int newRotate, ShapeId shapeId)
+{
     Shape shapeData = shapes[shapeId];
     int size = shapeData.size;
 
-    //判斷方塊有沒有不符合條件
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (shapeData.rotates[newRotate][i][j]) {
-                //判斷有沒有出去邊界
-                if (newX + j < 0 || newX + j >= CANVAS_WIDTH || newY + i < 0 || newY + i >= CANVAS_HEIGHT) {
+    // 判斷方塊有沒有不符合條件
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (shapeData.rotates[newRotate][i][j])
+            {
+                // 判斷有沒有出去邊界
+                if (newX + j < 0 || newX + j >= CANVAS_WIDTH || newY + i < 0 || newY + i >= CANVAS_HEIGHT)
+                {
                     return false;
                 }
-                //判斷有沒有碰到別的方塊
-                if (!canvas[newY + i][newX + j].current && canvas[newY + i][newX + j].shape != EMPTY) {
+                // 判斷有沒有碰到別的方塊
+                if (!canvas[newY + i][newX + j].current && canvas[newY + i][newX + j].shape != EMPTY)
+                {
                     return false;
                 }
             }
@@ -247,57 +276,140 @@ bool move(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int originalX, int original
     }
 
     // 移除方塊舊的位置
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (shapeData.rotates[originalRotate][i][j]) {
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (shapeData.rotates[originalRotate][i][j])
+            {
                 resetBlock(&canvas[originalY + i][originalX + j]);
             }
         }
     }
 
     // 移動方塊至新的位置
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (shapeData.rotates[newRotate][i][j]) {
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (shapeData.rotates[newRotate][i][j])
+            {
                 setBlock(&canvas[newY + i][newX + j], shapeData.color, shapeId, true);
             }
         }
     }
 
     return true;
-
 }
-int clearLine(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH]) {
-    for (int i = 0; i < CANVAS_HEIGHT; i++) {
-        for (int j = 0; j < CANVAS_WIDTH; j++) {
-            if (canvas[i][j].current) {
+int clearLine(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH])
+{
+    for (int i = 0; i < CANVAS_HEIGHT; i++)
+    {
+        for (int j = 0; j < CANVAS_WIDTH; j++)
+        {
+            if (canvas[i][j].current)
+            {
                 canvas[i][j].current = false;
             }
         }
     }
 
     int linesCleared = 0;
-    /*....日後會再更新*/
+
+    for (int i = CANVAS_HEIGHT - 1; i >= 0; i--)
+    {
+        //原本的code
+        bool isFull = true;
+        for (int j = 0; j < CANVAS_WIDTH; j++)
+        {
+            if (canvas[i][j].shape == EMPTY)
+            {
+                isFull = false;
+                break;
+            }
+        }
+        //新增的
+        if (isFull) {
+            linesCleared += 1;
+
+            for (int j = i; j > 0; j--)
+            {
+                for (int k = 0; k < CANVAS_WIDTH; k++)
+                {
+                    setBlock(&canvas[j][k], canvas[j - 1][k].color, canvas[j - 1][k].shape, false);
+                    resetBlock(&canvas[j - 1][k]);
+                }
+            }
+            i++;
+        }
+    }
     return linesCleared;
 }
 void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
 {
-    if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0]))
+    if (ROTATE_FUNC())
     {
-        state->y++;
+        int newRotate = (state->rotate + 1) % 4;
+        if (move(canvas, state->x, state->y, state->rotate, state->x, state->y, newRotate, state->queue[0]))
+        {
+            state->rotate = newRotate;
+        }
     }
-    else
+    else if (LEFT_FUNC())
     {
-        state->score += clearLine(canvas);
+        if (move(canvas, state->x, state->y, state->rotate, state->x - 1, state->y, state->rotate, state->queue[0]))
+        {
+            state->x -= 1;
+        }
+    }
+    else if (RIGHT_FUNC())
+    {
+        if (move(canvas, state->x, state->y, state->rotate, state->x + 1, state->y, state->rotate, state->queue[0]))
+        {
+            state->x += 1;
+        }
+    }
+    else if (DOWN_FUNC())
+    {
+        state->fallTime = FALL_DELAY;
+    }
+    else if (FALL_FUNC())
+    {
+        state->fallTime += FALL_DELAY * CANVAS_HEIGHT;
+    }
 
-        state->x = CANVAS_WIDTH / 2;
-        state->y = 0;
-        state->rotate = 0;
-        state->queue[0] = state->queue[1];
-        state->queue[1] = state->queue[2];
-        state->queue[2] = state->queue[3];
-        state->queue[3] = rand() % 7;
+    state->fallTime += RENDER_DELAY;
+    //100
+    //200
+    //300
+    //400
+    //500
+    while (state->fallTime >= FALL_DELAY)
+    {
+        state->fallTime -= FALL_DELAY;
+        if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0]))
+        {
+            state->y++;
+        }
+        else
+        {
+            state->score += clearLine(canvas);
 
+            state->x = CANVAS_WIDTH / 2;
+            state->y = 0;
+            state->rotate = 0;
+            state->queue[0] = state->queue[1];
+            state->queue[1] = state->queue[2];
+            state->queue[2] = state->queue[3];
+            state->queue[3] = rand() % 7;
+
+            //結束輸出
+            if (!move(canvas, state->x, state->y, state->rotate, state->x, state->y, state->rotate, state->queue[0]))
+            {
+                printf("\033[%d;%dH\x1b[41m GAME OVER \x1b[0m\033[%d;%dH", CANVAS_HEIGHT - 3, CANVAS_WIDTH * 2 + 5, CANVAS_HEIGHT + 5, 0);
+                exit(0);//結束遊戲
+            }
+        }
     }
     return;
 }
@@ -339,8 +451,8 @@ int main()
         }
     }
 
-
-    while (1) {
+    while (1)
+    {
         printCanvas(canvas, &state);
         logic(canvas, &state);
         Sleep(100);
